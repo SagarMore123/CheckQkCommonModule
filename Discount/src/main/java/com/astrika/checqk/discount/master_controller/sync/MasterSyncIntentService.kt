ab58843d038e865aefc5e09fde88367975b0b318
Sagar More <sagar.more@syspiretechnologies.com>
2021-02-02 11:29:12 +0530

    Bug resolved

--- Discount/src/main/java/com/astrika/checqk/discount/master_controller/sync/MasterSyncIntentService.kt
+++ Discount/src/main/java/com/astrika/checqk/discount/master_controller/sync/MasterSyncIntentService.kt
@@ -2,15 +2,17 @@ package com.astrika.checqk.discount.master_controller.sync
 
 import android.app.IntentService
 import android.content.Intent
-import com.astrika.checqk.discount.utils.Constants
 
 class MasterSyncIntentService : IntentService("MasterSyncIntentService") {
 
     override fun onHandleIntent(intent: Intent?) {
+        SyncData.syncData(this)
+/*
         if (intent?.getBooleanExtra(Constants.IS_SPLASH_MASTER, false) == true) {
             SyncData.splashSyncData(this)
         } else {
             SyncData.syncData(this)
         }
+*/
     }
 }
